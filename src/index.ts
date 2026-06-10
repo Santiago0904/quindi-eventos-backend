b14c6c4ef37674b199f3cc8e4821e41e4f7d5fcf
Santiago <santiago.bustos0409@gmail.com>
2026-06-09 23:17:00 -0500

    se implemento el login y el register del proyecto

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,7 @@
-import express from "express";
-import cors from "cors";
+import app from "./app.ts"
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-const PORT = 3000;
-
-app.get("/", (_req, res) => {
-  res.json({
-    mensaje: "Backend de QuindioEventos funcionando 🚀"
-  });
-});
+const PORT = 3000
 
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+  console.log(`Servidor corriendo en http://localhost:${PORT}`)
+})
